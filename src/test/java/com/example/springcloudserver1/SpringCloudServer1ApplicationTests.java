25fa850ac4b60768989c45ed1e04a6e247c5cfc0
kirsing <kirsing98@icloud.com>
2022-08-11 19:23:18 +0300

    initial commit

---
+++ src/test/java/com/example/springcloudserver1/SpringCloudServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
