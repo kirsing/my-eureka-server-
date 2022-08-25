edb0b02e20d138ee8cdc782e4b192a0e7da28f7b
kirsing <kirsing98@icloud.com>
2022-08-25 21:55:01 +0300

    updated

--- src/main/java/com/example/springcloudserver1/SpringCloudServer1Application.java
+++ src/main/java/com/example/springcloudserver1/SpringCloudServer1Application.java
@@ -3,6 +3,8 @@ package com.example.springcloudserver1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
